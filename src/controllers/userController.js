10e0dcf02f85da50630717a9f73901d04b449b8f
KhangNguyen220803 <nhkhang2101046@student.ctuet.edu.vn>
2024-10-10 16:29:30 +0700

    ver2_fix3

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -4,35 +4,6 @@ import userModel from "../services/userModel"
 const multer = require('multer');
 const path = require('path');
 
-// Cấu hình nơi lưu trữ và tên file cho ảnh
-const storage = multer.diskStorage({
-    destination: (req, file, cb) => {
-        cb(null, 'src/uploads/'); // Thư mục chứa file upload
-    },
-    filename: (req, file, cb) => {
-        cb(null, Date.now() + path.extname(file.originalname)); // Đặt tên file với timestamp
-    }
-});
-
-// Cấu hình multer với bộ lọc file
-const upload = multer({
-    storage: storage,
-    limits: { fileSize: 10000000 }, // Giới hạn kích thước file 1MB
-    fileFilter: (req, file, cb) => {
-        const filetypes = /jpeg|jpg|png/;
-        const extname = filetypes.test(path.extname(file.originalname).toLowerCase());
-        const mimetype = filetypes.test(file.mimetype);
-
-        if (mimetype && extname) {
-            return cb(null, true);
-        } else {
-            cb('Error: Images only!');
-        }
-    }
-});
-
-
-// ---------------------------------------------------------------------------------------------
 
 
 const getAllUser = async (req, res) => {
@@ -61,9 +32,7 @@ const showUserForm = async (req, res) => {
 
 const addUser = async (req, res) => {
     try {
-        // Dùng multer để upload ảnh
-        upload.single('hinhanh')(req, res, async (err) => {
-          
+     
 
             // Lấy dữ liệu từ body request
             let { username, password, fullname, address, sex, email} = req.body;
@@ -75,7 +44,7 @@ const addUser = async (req, res) => {
             // Chuyển hướng sau khi thành công
             res.redirect('/addUser');
 
-        });
+    
     } catch (error) {
         res.status(500).send({ message: 'Server Error' });
     }
@@ -92,11 +61,7 @@ const fillUserForm = async (req, res) => {
 
 const updateUser = async (req, res) => {
     try {
-        // Dùng multer để upload ảnh
-        upload.single('hinhanh')(req, res, async (err) => {
-            if (err) {
-                return res.status(400).send({ message: err });
-            }
+
 
             // Lấy dữ liệu từ body request
             let {  password, fullname, address, sex, email, username} = req.body;
@@ -107,7 +72,6 @@ const updateUser = async (req, res) => {
             // Chuyển hướng sau khi thành công
             res.redirect('../user');
 
-        });
     } catch (error) {
         res.status(500).send({ message: 'Server Error' });
     }
